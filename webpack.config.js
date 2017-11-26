d885b52ced769ed43fa7dc52904b931b7eb2d37a
Thomas CILES <thomas.ciles.perso@gmail.com>
2017-11-26 18:54:14 +0100

    add webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const Encore  = require('@symfony/webpack-encore');
+const path    = require('path');
+
+const assets_path   = path.resolve('./assets');
+const output_path   = (Encore.isProduction()) ? path.resolve('./build') : path.resolve('./build');
+const public_path   = (Encore.isProduction()) ? '/build' : '/build';
+const sass_path     = path.join(assets_path, './sass');
+const js_path       = path.join(assets_path, './js');
+
+Encore
+    // empty the outputPath dir before each build
+    .cleanupOutputBeforeBuild()
+
+    // directory where all compiled assets will be stored
+    .setOutputPath(output_path)
+
+    // what's the public path to this directory (relative to your project's document root dir)
+    .setPublicPath(public_path)
+
+    // will output as web/build/app.js
+    .addEntry('app', path.join(js_path, '/app.js'))
+
+    // will output as web/build/global.css
+    .addStyleEntry('style', path.join(sass_path, '/style.scss'))
+
+    // allow sass/scss files to be processed
+    .enableSassLoader()
+    .enablePostCssLoader()
+
+    .enableSourceMaps(!Encore.isProduction())
+
+    // create hashed filenames (e.g. app.abc123.css)
+    .enableVersioning(Encore.isProduction())
+;
+
+var config = Encore.getWebpackConfig();
+config.watchOptions = { poll: true, ignored: /node_modules/ };
+if (config.devServer) {
+    config.devServer.watchContentBase = true;
+}
+
+// export the final configuration
+module.exports = config;
